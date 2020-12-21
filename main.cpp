6fa4aa0b5c564b69af11b23be1106a2334c1d1cb
Ali Seif <65570362+aliseif321@users.noreply.github.com>
2020-12-21 21:43:47 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,48 +1,41 @@
 #include <iostream>
-#include "Seif.h"
-
+#include<Adjacency_matrix.h>
+#include<Neuronal_models.h>
 using namespace std;
 
 int main()
 {
-
-    //##############################################################
-    //####                                                      ####
-    //####               class Adjacency matrix                 ####
-    //####                                                      ####
-    //##############################################################
-    Number_of_nodes=10;                                              //[1,100]
-    Possibility_of_connection=0.9;                                  //[0,1]
+    Number_of_nodes=10;
+    Possibility_of_connection=0.5;
+    Address="E:/programs/add corent/first/";
+    Changer=5;
     class Adjacency adj;
-    adj.Build_matrix();
-    //adj.Show_Binary();
-    adj.Print_Binary();
+    adj.Build_matrix_zero();
+    adj.Erdos_Renyi();
+    adj.Show_Binary();
     adj.Print_Network();
-    //##############################################################
-    //####                                                      ####
-    //####            class Hodgkin_Huxley Excitatory           ####
-    //####                                                      ####
-    //##############################################################
-    External_electric_current_HH=9.7;                                  //[9.7,]
-    Storage_start_time=2000;                                        //The execution time of the program is from zero
-    Length_of_steps=0.1;                                            //(0,1] ms
-    Final_time=2500;                                                //ms
-    Coupling_strength_HH=0.05;                                         //[0.0003,]
-    class Hodgkin_Huxley HH;
-    HH.Start();
-    HH.Print_Data();
-    //HH.Show_Data();
-    //##############################################################
-    //####                                                      ####
-    //####             class Wang-Buzsaki Inhibitory            ####
-    //####                                                      ####
-    //##############################################################
-    External_electric_current_WB=5;                                  //[0.16,]
-    Coupling_strength_WB=5;                                         //[0,0.3]
-    class Wang_Buzsaki WB;
-    WB.Start();
-    WB.Print_Data();
-    //WB.Show_Data();
+    adj.Print_Binary();
+    //adj.Build_array_rand();
+    //adj.Build_onelink_step();
 
+    for (int i=0 ; i<Number_of_nodes;i++){
+       for (int j=0 ; j<Number_of_nodes;j++){
+            Matrix_N[i][j]=Matrix[i][j];
+       }
+    }
+
+    Number_of_nodes_N=10;
+    Changer_N=5;
+    Address="E:/programs/add corent/first/";
+    Length_of_steps=0.1;                                            //(0,1] ms
+    Final_time=2500;
+    External_electric_current_HH=9.741;//9.741                                  //[9.7,]
+    Coupling_strength_HH=0.001;//                                         //[0.0003,]
+    class Hodgkin_Huxley hh;
+    hh.Beginning();
+    //hh.openfile();
+    hh.Print_Data();
+
+    cout << "\nrun done" << endl;
     return 0;
 }
